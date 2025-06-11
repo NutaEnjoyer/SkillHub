c60618729089f596b5d67d32cd4a968ab65447e7
Max <mdolgov322@gmail.com>
2025-06-12 02:23:44 +0400

    chore: migrate database from SQLite to PostgreSQL

--- skillhub/skillhub/settings.py
+++ skillhub/skillhub/settings.py
@@ -10,14 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+from datetime import timedelta
 from pathlib import Path
 
-from datetime import timedelta
-
-import os
-
 from decouple import Config, RepositoryEnv
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,11 +93,14 @@ WSGI_APPLICATION = "skillhub.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config.get("DB_NAME"),
+        "USER": config.get("DB_USER"),
+        "PASSWORD": config.get("DB_PASSWORD"),
+        "HOST": config.get("DB_HOST"),
+        "PORT": config.get("DB_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
